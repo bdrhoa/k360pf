2e7a82fbccf290157f8b88292a8447e794c5348d
Brad Rhoads <bdrhoa@gmail.com>
2025-05-10 10:06:51 -0600

    `Refactor: Replace Singleton with Scoped instance of SignatureVerifier`

--- dotnet/KountWebhook/Program.cs
+++ dotnet/KountWebhook/Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
-builder.Services.AddSingleton<KountJwtAuth.Services.SignatureVerifier>();
+builder.Services.AddScoped<KountJwtAuth.Services.ISignatureVerifier, KountJwtAuth.Services.SignatureVerifier>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
