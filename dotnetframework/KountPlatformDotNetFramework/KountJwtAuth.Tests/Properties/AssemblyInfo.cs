fd61a2e9460980597b41c82a6ea824f34fed80f4
Brad Rhoads <bdrhoa@gmail.com>
2025-06-23 19:18:24 -0600

    `Added KountJwtAuth.Tests project to the solution`

---
+++ dotnetframework/KountPlatformDotNetFramework/KountJwtAuth.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KountJwtAuth.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KountJwtAuth.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("97941aeb-c325-48b3-be75-acbac5ffbdc3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
