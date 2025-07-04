1fa91f0e6a79c2bbb9a3dc048fa66fe91cfa38f3
Brad Rhoads <bdrhoa@gmail.com>
2025-07-03 18:51:53 -0600

    `Added test for VerifySignature with valid signature (doesn't work)`

--- dotnet/KountJwtAuth.Tests/SignatureVerifierTests.cs
+++ dotnet/KountJwtAuth.Tests/SignatureVerifierTests.cs
@@ -117,5 +117,22 @@ namespace KountJwtAuth.Tests.Services
             Assert.Throws<ArgumentException>(() =>
                 _verifier.VerifySignature(badSignature, timestamp, payload));
         }
+
+                [Fact]
+        public void VerifySignature_WithValidSignature_ReturnsTrue()
+        {
+            var rawJson = "{\"id\":\"321bc10b-b49d-4b75-8caf-07fd11cc4ad2\",\"eventType\":\"Order.StatusChange\",\"apiVersion\":\"v1\",\"clientId\":\"928737379799093\",\"kountOrderId\":\"0XWFM3QPGN5G1F7C\",\"merchantOrderId\":\"1754456650015\",\"orderCreationDate\":\"2025-07-01T00:57:29.849Z\",\"eventDate\":\"2025-07-01T00:58:20Z\",\"fieldName\":\"status\",\"oldValue\":\"REVIEW\",\"newValue\":\"DECLINE\",\"channel\":\"WEBHTEST\"}";
+            var payload = Encoding.UTF8.GetBytes(rawJson);
+            //var timestamp = "2025-07-01T00:58:21.0000000Z";
+            var timestamp = "2025-07-01T00:58:21Z";
+            var fixedTime = DateTime.Parse(timestamp).ToUniversalTime();
+
+            // You must set the KOUNT_PUBLIC_KEY environment variable before running this test
+            var signature = "Qth86BE0OTQ/AU/R9o121MaE6lYJAWQwQzfcLGfCdi4kn7VabAptF/j29f0dUP2Sgk2eqcK21hPMsr5bptEZLQNoMbX/19N/9vePJBkbTwwHTV2QSOzOfAfLsZFYbBQFYrEphAwsmipFHsIO3d0+jjJcF3Kf+zzBAi5CZ7IpoKLttjmf9xaDgfYHcDcvx3jbkuEaOyshwnedLEx5SD6Uk1WW2WIf/gpQdK2+jBjgryn1L7LNytyFt7PmC+gHWqZSgpAaOhymy3GoL5utzvYphGmkgLvA1CQB0RojHV69DnQ2DQ47pfIaUdLPeyGw8mfB26+ZMScf8LyMjt4yhPDg3yfc8mNAcBLpXzaBb3RRdkRgXvq2R0/D/19dqRhl0JuAB9IWmM/eF44uPBJ253hYzgDe5uULAisoPr2ual4JToJu3NDzjY3sU0RPEFzNyY46JzjEabtrIBu+HfEN4T+QgHccfutHf9QZb6YHUkax5GqLyerZBdOyVnCqDYnqAHfnOePAKEi2DYwPpqQPJfNu3vI17M+Y3liFonaE+J7Upt9xtFsI1vrpr31eok/xZjLOsIv0AdkuLb7SWpt6JmG+UYP7ijNp6eLr8kbHpXoQbVlBcfggnwrFXvIjcR9/bYZ0ukUkQcU/hnSZDzA83Xr692CEb4+VlAIEqcDBsNA5AkU=";
+
+            var result = _verifier.VerifySignature(signature, timestamp, payload, fixedTime);
+            Assert.True(result);
+        }
     }
 }
+
