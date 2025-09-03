53b799bed211f87c1ffded386725f2a8443d6a4e
Brad Rhoads <bdrhoa@gmail.com>
2025-09-03 13:28:12 -0500

    Update AUTH tests.

--- dotnet/KountJwtAuth.Tests/SignatureVerifierTests.cs
+++ dotnet/KountJwtAuth.Tests/SignatureVerifierTests.cs
@@ -69,7 +69,6 @@ namespace KountJwtAuth.Tests.Services
         }
 
         [Fact]
-//this doesn't work yet.
         public void VerifySignature_TimestampTooOld_Throws()
         {
             var timestamp = DateTime.UtcNow.AddMinutes(-10).ToString("o");
@@ -119,21 +118,18 @@ namespace KountJwtAuth.Tests.Services
                 _verifier.VerifySignature(badSignature, timestamp, payload));
         }
 
-                [Fact]
+        [Fact]
         public void VerifySignature_WithValidSignature_ReturnsTrue()
         {
-            var rawJson = "{\"id\":\"321bc10b-b49d-4b75-8caf-07fd11cc4ad2\",\"eventType\":\"Order.StatusChange\",\"apiVersion\":\"v1\",\"clientId\":\"928737379799093\",\"kountOrderId\":\"0XWFM3QPGN5G1F7C\",\"merchantOrderId\":\"1754456650015\",\"orderCreationDate\":\"2025-07-01T00:57:29.849Z\",\"eventDate\":\"2025-07-01T00:58:20Z\",\"fieldName\":\"status\",\"oldValue\":\"REVIEW\",\"newValue\":\"DECLINE\",\"channel\":\"WEBHTEST\"}";
+            //var rawJson = "{\"id\":\"d27a75dc-6a13-40ce-83c7-d9ab963ab408\", \"eventType\":\"Order.StatusChange\", \"apiVersion\":\"v1\", \"clientId\":\"928737379799093\", \"kountOrderId\":\"V2D1MDKZXVYJXVZZ\", \"merchantOrderId\":\"d121ea2210434ffc8a90daff9cc97e76\", \"orderCreationDate\":\"2025-05-10T18:25:09.045Z\", \"eventDate\":\"2025-07-04T01:46:13Z\", \"fieldName\":\"status\", \"oldValue\":\"REVIEW\", \"newValue\":\"DECLINE\", \"channel\":\"DOTNET\"}";
+            var rawJson = "{\"id\":\"d27a75dc-6a13-40ce-83c7-d9ab963ab408\", \"eventType\":\"Order.StatusChange\", \"apiVersion\":\"v1\", \"clientId\":\"928737379799093\", \"kountOrderId\":\"V2D1MDKZXVYJXVZZ\", \"merchantOrderId\":\"d121ea2210434ffc8a90daff9cc97e76\", \"orderCreationDate\":\"2025-05-10T18:25:09.045Z\", \"eventDate\":\"2025-07-04T01:46:13Z\", \"fieldName\":\"status\", \"oldValue\":\"REVIEW\", \"newValue\":\"DECLINE\", \"channel\":\"DOTNET\"}";
             var payload = Encoding.UTF8.GetBytes(rawJson);
-            //var timestamp = "2025-07-01T00:58:21.0000000Z";
-            var timestamp = "2025-07-01T00:58:21Z";
+            var timestamp = "2025-07-04T01:46:15Z";
+            var signature = "Wgpzu4WT48OLo8EAaSXBhEWREv2dp0jsZiXIoo5+AHLHR36XC5TpTc1gXnD2kvyyF6LzMkpp3Zu2oye83Iui5yr6y5PWndKhsIxSGCjCu9Jeizjaw1cafoemGpejZIo+6k9IVitEc7znuZ4GjBz+SQHuVlClFtuaANz0fnPQ37WGnyEsEVHrmYy7Dwlsof8Ajy2ALK+5xtXanSxah9Gs+XQT0klSjw9rsOfJVgd/Da8/FInig0jrQ3mQdcflycPuANRMg06CmWv40ZM04orAX/BnpWvIj2FAMqiJVyca6SFEtXnHXuqntI9BbZgPC+mHkjzvFPkOFES4LOPzKdMoQQLXFxuuGNGtJCGweEwHCB5BrO0gzEUJH7C+E4bfBxGWCZ74KUCwncD/am7/NPEooMEjYLo6lV/Pk3+ECv7Q1hpB7s425iSMN872wjS/0tDPKGlyc4jR87hyNVTwIIQkYY+HSuJCpSFSK/ukrsYJfZ3g+8D4Tsd6kx9CHWj5nMlL4dX/pyLgqd/gAui3xBzgjAbsu5ymRzIwAuq33klVg9hrFCpAfX3TMtPirl7sgK7dXfoRnDyJBFVTyNaaWBkoxbqvHkeSHBnz0b4H3QN/fyHf88vGxCYkEJ1U4Uz9inJ0U+VLV3ISiK8vn7BBDTu4T97RB2o9J7+9JojQDxCOR7M=";
             var fixedTime = DateTime.Parse(timestamp).ToUniversalTime();
-
-            // You must set the KOUNT_PUBLIC_KEY environment variable before running this test
-            var signature = "Qth86BE0OTQ/AU/R9o121MaE6lYJAWQwQzfcLGfCdi4kn7VabAptF/j29f0dUP2Sgk2eqcK21hPMsr5bptEZLQNoMbX/19N/9vePJBkbTwwHTV2QSOzOfAfLsZFYbBQFYrEphAwsmipFHsIO3d0+jjJcF3Kf+zzBAi5CZ7IpoKLttjmf9xaDgfYHcDcvx3jbkuEaOyshwnedLEx5SD6Uk1WW2WIf/gpQdK2+jBjgryn1L7LNytyFt7PmC+gHWqZSgpAaOhymy3GoL5utzvYphGmkgLvA1CQB0RojHV69DnQ2DQ47pfIaUdLPeyGw8mfB26+ZMScf8LyMjt4yhPDg3yfc8mNAcBLpXzaBb3RRdkRgXvq2R0/D/19dqRhl0JuAB9IWmM/eF44uPBJ253hYzgDe5uULAisoPr2ual4JToJu3NDzjY3sU0RPEFzNyY46JzjEabtrIBu+HfEN4T+QgHccfutHf9QZb6YHUkax5GqLyerZBdOyVnCqDYnqAHfnOePAKEi2DYwPpqQPJfNu3vI17M+Y3liFonaE+J7Upt9xtFsI1vrpr31eok/xZjLOsIv0AdkuLb7SWpt6JmG+UYP7ijNp6eLr8kbHpXoQbVlBcfggnwrFXvIjcR9/bYZ0ukUkQcU/hnSZDzA83Xr692CEb4+VlAIEqcDBsNA5AkU=";
 
             var result = _verifier.VerifySignature(signature, timestamp, payload, fixedTime);
             Assert.True(result);
         }
     }
 }
-
